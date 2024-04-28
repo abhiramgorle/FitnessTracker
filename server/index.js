ccd31eaa03024dfb1cf48248e1a1ad5d39705cbb
abhiramgorle <80030901+abhiramgorle@users.noreply.github.com>
2024-04-28 19:10:01 +0530

    somethinsg

--- server/index.js
+++ server/index.js
@@ -39,11 +39,11 @@ const connectDB = () => {
       console.error(err);
     });
 };
-
+const port = process.env.PORT || 8080
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(port, () => console.log(`Server started on port ${port}`));
   } catch (error) {
     console.log(error);
   }
